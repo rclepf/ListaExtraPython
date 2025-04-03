87ce73adf32e25d6d26f893b0a0635971430f4b2
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-04-03 19:22:26 -0300

    Create 09.py

---
+++ 09.py
@@ -0,0 +1,5 @@
+# Desconto na Loja
+preco = float(input("Digite o preço do produto: "))
+desconto = preco * 0.10
+novo_preco = preco - desconto
+print(f"O novo preço com 10% de desconto é R$ {novo_preco:.2f}")
