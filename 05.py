546c0adcd08ae992458a3a88934378b3915aa6f4
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-04-03 19:19:41 -0300

    Create 05.py

---
+++ 05.py
@@ -0,0 +1,6 @@
+# Média Simples
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+n3 = float(input("Digite o terceiro número: "))
+media = (n1 + n2 + n3) / 3
+print(f"A média dos números é {media:.2f}")
